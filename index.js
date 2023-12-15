0038fd6b7b7802fe8bee6adf4ceb49eefd2a255e
karimi <karimidorcas1@gmail.com>
2023-12-15 15:45:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let ride;
+  
+    if(distance <= 400 ){
+      ride = 'This one is on me!';
+    } else if(distance > 2000 && distance < 2500){
+      ride ="I will gladly take your thirty bucks.";
+    } else {
+      ride = 'No can do.';
+    }
+    return ride;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  scuberGreetingForFeet(199)
+  scuberGreetingForFeet(2001)
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  let country = city == 'NYC'? 'Ok, sounds good.': "No go."
+  return country;
+
+};
+
+function switchOnCharmFromTip(tip){
+switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+}
 }
